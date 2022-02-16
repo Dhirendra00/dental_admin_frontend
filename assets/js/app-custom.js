96da6180d5608a33559defc5fb6767196d00a87c
Dhirendra Baduwal <dhirendra@Dhirendras-MacBook-Pro.local>
2022-02-16 14:29:46 +0000

    added

---
+++ assets/js/app-custom.js
@@ -0,0 +1,22 @@
+/*
+Document: app-custom.js
+Author: Bylancer
+Description: Write your custom code here
+*/
+
+// Below is an example of function and its initialization
+var AppCustom = function() {
+	var showAppName = function() {
+		
+	};
+	return {
+		init: function() {
+			showAppName();
+		}
+	}
+}();
+
+// Initialize AppCustom when page loads
+jQuery( function() {
+	AppCustom.init();
+});
